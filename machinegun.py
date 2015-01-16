c91fd980fbbfd31e9fb441f4120dc97b4d5e1fbc
Damien <damien@dam.io>
2015-01-16 07:12:35 -0500

    170000 rows

--- machinegun.py
+++ machinegun.py
@@ -4,5 +4,6 @@ pids = []
 for i in range(int(sys.argv[1])):
     pid = subprocess.Popen(["python", "sante.py"], stdout=sys.stdout, stderr=sys.stderr).pid
     pids.append(pid)
+    time.sleep(3)
 
 import pdb;pdb.set_trace()
